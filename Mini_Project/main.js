5216b1872b2d5c3d04a0324b8da69e7b325fa4b0
Satvik Singh <=>
2022-02-14 23:23:42 +0530

    used fetch api

--- Mini_Project/main.js
+++ Mini_Project/main.js
@@ -15,3 +15,10 @@ minusButton.addEventListener('click',function(event){
     toggledBar.classList.toggle('toggled-bar-hide');
 })
 
+async function getImage(url){
+    let response = await fetch(url);
+    let blob = await response.blob(); // download as Blob object
+    document.getElementById('fetched-image').src = URL.createObjectURL(blob);
+}
+
+getImage('https://upload.wikimedia.org/wikipedia/commons/f/fb/Facebook_icon_2013.svg')
